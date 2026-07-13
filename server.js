eef0fe8277ee84e11bf51ef5e3d6dbc63499e894
Sanskruti Chopade <sanskruti@example.com>
2026-07-13 16:11:33 +0530

    Refactor server.js to use ES module syntax for imports and clean up commented code

--- server.js
+++ server.js
@@ -1,14 +1,24 @@
-const express= require('express');
-const path =require('path');
-const post=require('./routes/posts');
+import express from 'express';
+//const express= require('express');
+
+import path from 'path';
+//const path =require('path');
+
+import posts from'./routes/posts.js';
+//const post=require('./routes/posts');
+
 const port = process.env.PORT || 8000;
 
 const app=express();
 
-app.use((req, res, next) => {
-  console.log('Incoming:', req.method, req.url);
-  next();
-});
+//Body parse middleware-->take care to send json 
+app.use(express.json());
+app.use(express.urlensoded({extended:false}));
+
+// app.use((req, res, next) => {
+//   console.log('Incoming:', req.method, req.url);
+//   next();
+// });
 
 //setup static folder 
 //app.use(express.static(path.join(__dirname, 'public'))); 
@@ -20,12 +30,6 @@ app.use((req, res, next) => {
 // app.get('/about',(req, res)=>{
 //     res.sendFile(path.join(__dirname, 'public', 'about.html'));
 // });
-
-
-
-
-//Get single posts (fixed)
-// NOTE: Keep only one handler for /api/posts
 
 //Routes
 app.use('/api/posts', posts);
