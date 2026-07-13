345ee08678f517461fb6899256387ce32922595b
Sanskruti Chopade <sanskruti@example.com>
2026-07-13 16:45:49 +0530

    Refactor posts API routes to include logging middleware and improve error handling for post updates and deletions

--- routes/posts.js
+++ routes/posts.js
@@ -25,9 +25,16 @@ app.get('/',(req, res)=>{
     }
 });
 
+const logger =(req, res, next) => {
+    console.log(`${req.method} ${req.protocol}://${req.get('host')}${req.originalUrl}`);
+ next();
+};
+
+
+
 
 //get all posts
-Router.get('/', (req,res) =>{
+Router.get('/',logger, (req,res) =>{
     const limit=parseInt(req,query.limit);
     if(!isNaN(limit) && limit>0){
         return res.status(200).json(posts.slice(0,limit));
@@ -54,13 +61,43 @@ router.post('/',(req, res) => {
         title:req.body.title,
     };
     if(!newPost.title){
-        return res.sendStatus(400).json({msg:'Plaease add the title'});
+        return res.sendStatus(400).json({msg:'Plaease include the title'});
     }
     //console.log(req.body);
 posts.push(newPost);
     res.status(201).json(posts);
 });
 
+//Update Post
+router.put('/:id',(req,res)=> {
+    const id =parseInt(reqparams.id);
+    const post =posts.find((post) => post.id);
+    
+    if(!post){
+        return res
+        .sendStatus(404)
+        .json({msg:`A post with the id of ${id} was not not found` });
+
+    }
+post.title= req.body.title;
+res.ststus(200).json(posts);
+});
+
+
+//Delete Post 
+router.delete('/:id',(req,res)=> {
+    const id =parseInt(reqparams.id);
+    const post =posts.find((post) => post.id);
+    
+    if(!post){
+        return res
+        .sendStatus(404)
+        .json({msg:`A post with the id of ${id} was not not found` });
+    }
+    posts=posts.filter((post) => post.id !== id);
+
+res.ststus(200).json(posts);
+});
 
 export default router;
 //module.exports=Router;
