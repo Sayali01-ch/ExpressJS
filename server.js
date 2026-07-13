8c5a0e470efe19c4d5d1ce1e1f503b53aebe08c0
Sanskruti Chopade <sanskruti@example.com>
2026-07-13 11:18:35 +0530

    Implement pagination for posts API endpoint

--- server.js
+++ server.js
@@ -20,8 +20,14 @@ let posts=[
 {id:3, title:'Post Three'}
 ];
 
+//get limited outputs
 app.get('/api/posts',(req, res)=>{
-res.json(posts);
+    const limit=parseInt(req.query.limit);
+    if(!isNaN(limit)&& limit>0){
+        res.json(posts.slice(0,limit));
+    }else{
+        res.json(posts);
+    }
 });
 
 app.get('/api/posts/:id',(req, res)=>{
