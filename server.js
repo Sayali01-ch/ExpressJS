bda53f6c52f2727c5add8b9668105feac971dca6
Sanskruti Chopade <sanskruti@example.com>
2026-07-14 11:33:13 +0530

    Add notFound middleware for handling 404 errors

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ import posts from'./routes/posts.js';
 //const post=require('./routes/posts');
 import logger from './middleware/logger.js'
 import errorHandler from './middleware/error.js';
+import notFound from './middleware/notFound.js';
+
 const port = process.env.PORT || 8000;
 
 const app=express();
@@ -38,13 +40,13 @@ app.use(logger);
 
 //Routes
 app.use('/api/posts', posts);
-app.use((req, res, next) => {
-    const error= new Error('Not Found');
-    error.status=404;
-    next(error);
-});
-
+// app.use((req, res, next) => {
+//     const error= new Error('Not Found');
+//     error.status=404;
+//     next(error);
+// });
 
 //Error handler 
+app.use(notFound);
 app.use(errorHandler);
 app.listen(port, () => console.log(`Server is running on port ${port}`));
