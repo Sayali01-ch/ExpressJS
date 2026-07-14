18a204ab6e42efddf4539301ff117cb8bd44539d
Sanskruti Chopade <sanskruti@example.com>
2026-07-14 11:41:16 +0530

    Refactor logger middleware to implement method-specific color coding for improved log visibility

--- middleware/logger.js
+++ middleware/logger.js
@@ -1,11 +1,24 @@
 import colors from 'colors';
 
-const logger =(req, res, next) => {
-     console.log(`${req.method} ${req.protocol}://${req.get('host')}${req.originalUrl}`[
-        'red'
-     ]
-    );
-  next();
- };
+const logger = (req, res, next) => {
+  const methodColors={
+   GET:'green',
+   POST:'blue',
+   PUT :' yellow',
+   DELETE: 'red'
+  }
+const color =methodColors[req.method] || white;
 
- export default logger 
+   console.log( 
+   `${req.method} ${req.protocol}://${req.get('host')}${req.originalUrl}`[
+   color
+  ]
+  );
+  // colors package adds ANSI color methods (colors.red, colors.blue, etc.)
+  //console.log(colors.red(url));
+
+  next();
+};
+
+export default logger;
+
