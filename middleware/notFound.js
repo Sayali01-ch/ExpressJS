32789c544dbd8411351b7c0d54ca4f0abfdd5014
Sanskruti Chopade <sanskruti@example.com>
2026-07-15 11:14:51 +0530

    Fix typo in error status assignment in notFound middleware

--- middleware/notFound.js
+++ middleware/notFound.js
@@ -1,7 +1,7 @@
 const notFound =(req,res,next)=>
 {
-    const error =new Error('Not Found');
-    error.ststus=404;
+    const error = new Error('Not Found');
+    error.status = 404;
     next(error);
 
 };
