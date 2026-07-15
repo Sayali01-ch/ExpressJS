2f1ae112b245712cdd5c577d7d814a2a58a148e4
Sanskruti Chopade <sanskruti@example.com>
2026-07-15 11:07:45 +0530

    improved structure

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 import express from 'express';
 //const express= require('express');
-
 import path from 'path';
 //const path =require('path');
-
+import {fileURLToPath} from 'url';
 import posts from'./routes/posts.js';
 //const post=require('./routes/posts');
 import logger from './middleware/logger.js'
@@ -11,7 +10,11 @@ import errorHandler from './middleware/error.js';
 import notFound from './middleware/notFound.js';
 
 const port = process.env.PORT || 8000;
+//Get the directory name 
+const __filename= fileURLToPath(import.meta.url);
+const __dirname =path.dirname(__filename);
 
+//console.log(__filename);
 const app=express();
 
 //Body parse middleware-->take care to send json 
@@ -21,32 +24,14 @@ app.use(express.urlencoded({ extended: false }));
 //Logger middleware 
 app.use(logger);
 
-
-// app.use((req, res, next) => {
-//   console.log('Incoming:', req.method, req.url);
-//   next();
-// });
-
 //setup static folder 
-//app.use(express.static(path.join(__dirname, 'public'))); 
-
-// app.get('/',(req, res)=>{
-//     res.sendFile(path.join(__dirname, 'public', 'index.html'));
-// });
-
-// app.get('/about',(req, res)=>{
-//     res.sendFile(path.join(__dirname, 'public', 'about.html'));
-// });
+app.use(express.static(path.join(__dirname, 'public')));
 
 //Routes
 app.use('/api/posts', posts);
-// app.use((req, res, next) => {
-//     const error= new Error('Not Found');
-//     error.status=404;
-//     next(error);
-// });
 
 //Error handler 
 app.use(notFound);
 app.use(errorHandler);
+
 app.listen(port, () => console.log(`Server is running on port ${port}`));
